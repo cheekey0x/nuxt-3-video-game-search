875829b250013417eed38cf0b0100305ae2a3de7
cheekey0x <cheekey0x@outlook.com>
2024-01-16 16:20:54 -0500

    add result render conditions, footer & main section content and site styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,4 +7,18 @@
   <main>
     <slot/>
   </main>
+  
+  <hr class="mb-7">
+
+  <div class="text-center">
+    <h2 class="mb-5">
+      Built with Nuxt 3 by <a href="https://www.brandonwinger-air.com" target="_blank" class="font-semibold">Brandon Winger-Air</a> using <a href="https://www.giantbomb.com/api/" target="_blank" class="font-semibold">external API</a>.
+    </h2>
+
+    <hr width="325" class="mx-auto mb-4">
+
+    <h3 class="mb-7">
+      <a href="https://github.com/BrandonWingerAir/nuxt-3-video-game-search" target="_blank">GitHub 💻</a>
+    </h3>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,50 @@
 <template>
   <div>
-    <div class="flex flex-col py-10">
-      <div class="mb-10">
-        <h2 class="text-2xl font-bold text-center">Video Games List</h2>
+    <div class="flex flex-col pt-10">
+      <div class="mb-10 text-center">
+        <h1 class="text-3xl font-bold">Video Games</h1>
       </div>
-      <div class="flex justify-center mb-8">
-        <div class="border rounded-md border-neutral-400">
+      <div class="flex justify-center mb-10">
+        <div class="border rounded-md border-neutral-400 mr-1">
           <input 
             class="px-2 py-1 border-0 min-w-64"
             v-model="searchTerm"
             type="text"
             placeholder="Game Title..."
+            @click="showResults = !showResults && hideSuggestions ? hideSuggestions = !hideSuggestions : hideSuggestions = !hideSuggestions"
           />
-          <div v-if="data?.results[0] && !showResults" class="px-2 pb-2">
+          <div v-if="data?.results[0] && !hideSuggestions" class="px-2 pb-2 absolute z-20 bg-white border min-w-64">
             <ul v-for="game in data?.results">
               <li class="py-1">{{ game.name }}</li>
             </ul>
           </div>
         </div>
-        <button @click="showResults = !showResults" class="h-9 px-4 bg-cyan-500 text-white font-semibold rounded-lg">
+        <button @click="!showResults ? showResults = !showResults : showResults = showResults; hideSuggestions = true" class="h-9 px-4 bg-cyan-500 text-white font-semibold rounded-lg">
           Search
         </button>
       </div>
-      <div v-if="showResults" class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 self-center gap-x-10 gap-y-10 mb-10">
-        <div v-for="game in data?.results">
-          <GameCard :game="game"></GameCard>
+
+      <hr class="mb-10">
+
+      <div class="z-0 self-center">
+        <div v-if="showResults" class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-10 gap-y-10 mb-10">
+          <div v-for="game in data?.results">
+            <GameCard :game="game"></GameCard>
+          </div>
+        </div>
+        <div v-if="!showResults" class="mb-10 text-center">
+          <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-10 gap-y-10 mb-10">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+            <img src="/254x356-gamepad.png" alt="">
+          </div>
         </div>
       </div>
     </div>
@@ -45,4 +65,5 @@ const url = computed(() => {
 const { data } = await useFetch<Response>(url);
 
 const showResults = ref(false);
+const hideSuggestions = ref(true);
 </script>
