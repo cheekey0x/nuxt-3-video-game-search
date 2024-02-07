1b2757b0d613f77057ddb770d8e313ded3e8b83c
cheekey0x <cheekey0x@outlook.com>
2024-02-07 16:10:47 -0500

    site header navigation menu structure & classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
-  <nav class="h-20 flex justify-between bg-white border-b-2 border-black pl-4 md:pl-6 pr-2 md:pr-4 py-4 text-white">
-    <img src="/gaming-pc-logo.png" alt="gaming logo" class="w-14 h-14 self-center">
+  <nav class="h-20 flex bg-white border-b-2 border-black pl-4 md:pl-6 pr-2 md:pr-4 py-4 text-white">
+    <div class="flex w-1/3 justify-start">
+      <img src="/gaming-pc-logo.png" alt="gaming logo" class="w-14 h-14 self-center">
+    </div>
 
-    <NuxtLink to="/resources" class="h-11 px-4 pt-2 ml-11 bg-emerald-500 hover:bg-emerald-600 border border-neutral-800 rounded-lg self-center">
-      Learn<span class="hidden md:inline-block">ing Resources</span>
-    </NuxtLink>
-    <NuxtLink to="/" class="h-11 px-4 pt-2 ml-11 bg-slate-500 border border-neutral-800 rounded-lg self-center">
-      Home Page
-    </NuxtLink>
+    <div class="flex w-1/3 justify-center">
+      <NuxtLink to="/resources" class="h-11 px-4 pt-2 bg-emerald-500 hover:bg-emerald-600 border border-neutral-800 rounded-lg self-center">
+        Learn<span class="hidden md:inline-block">ing Resources</span>
+      </NuxtLink>
+    </div>
+    <div class="flex w-1/3 justify-end">
+      <NuxtLink to="/" class="h-11 px-4 pt-2 bg-slate-500 border border-neutral-800 rounded-lg self-center">
+        Home Page
+      </NuxtLink>
+    </div>
   </nav>
   <main>
     <slot/>
